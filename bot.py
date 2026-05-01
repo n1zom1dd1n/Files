7df2f97a71fce6e6cf817e4cc49102b7442a0902
n1zom1dd1n <muboraknizomiddin0@gmail.com>
2026-05-01 11:43:49 +0500

    Refactor: use env vars and fix requirements

--- bot.py
+++ bot.py
@@ -2,10 +2,10 @@ import os
 from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 
-TOKEN = "8674997924:AAHT7w5uf8P5tCMMz0ptqFjOSaLaKJBTYTE"
+TOKEN = os.getenv("8674997924:AAHT7w5uf8P5tCMMz0ptqFjOSaLaKJBTYTE")
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text("Bot is working!")
+    await update.message.reply_text("Bot is working!Nigga")
 
 app = ApplicationBuilder().token(TOKEN).build()
 app.add_handler(CommandHandler("start", start))
